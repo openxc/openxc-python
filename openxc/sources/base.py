bf6f140d0ef7154a17fc1f4197603a4a8a4cbc45
Christopher Peplin <chris.peplin@rhubarbtech.com>
2012-09-28 19:37:53 -0400

    When a message is received, give it to a callback function.

--- openxc/sources/base.py
+++ openxc/sources/base.py
@@ -4,21 +4,34 @@ from openxc.formats.json import JsonFormatter
 
 
 class DataSource(object):
-    def __init__(self):
+    def __init__(self, callback):
         self.messages_received = 0
         self.good_messages = 0
         self.total_bytes_received = 0
-        self.message_buffer = ""
+        self.callback = callback
 
     def start(self):
         self.thread = threading.Thread(target=self.run)
         self.thread.start()
 
     def run(self):
+        message_buffer = ""
         while True:
-            self.message_buffer += self.read()
-            while self._parse_message():
-                continue
+            message_buffer += self.read()
+            while True:
+                message, message_buffer, received = self._parse_message(
+                        message_buffer)
+                if received and message is None and self.messages_received == 0:
+                    # assume the first message will be caught mid-stream and
+                    # thus will be corrupted
+                    self.messages_received -= 1
+                else:
+                    self.good_messages += 1
+                    self.total_bytes_received += len(message)
+                    self.callback(message)
+
+                if received:
+                    self.messages_received += 1
 
     def read(self, num_bytes=None, timeout=None):
         raise NotImplementedError("Don't use DataSource directly")
@@ -45,25 +58,23 @@ class DataSource(object):
                 pass
         return value
 
-    def _parse_message(self):
-        if "\n" in self.message_buffer:
-            message, _, remainder = self.message_buffer.partition("\n")
+    def _parse_message(self, message_buffer):
+        """If a message can be pasred from the given buffer, return it and
+        remove it.
+
+        Returns the message if one could be parsed, otherwise None, and the
+        remainder of the buffer.
+        """
+        parsed_message = None
+        received = False
+        remainder = message_buffer
+        if "\n" in message_buffer:
+            received = True
+            message, _, remainder = message_buffer.partition("\n")
             try:
                 parsed_message = JsonFormatter.deserialize(message)
                 if not isinstance(parsed_message, dict):
                     raise ValueError()
             except ValueError:
-                if self.messages_received == 0:
-                    # assume the first message will be caught mid-stream and
-                    # thus will be corrupted
-                    self.messages_received -= 1
-            else:
-                self.good_messages += 1
-                self.total_bytes_received += len(message)
-
-                return parsed_message
-            finally:
-                self.message_buffer = remainder
-                self.messages_received += 1
-            return True
-        return False
+                pass
+        return parsed_message, remainder, received
