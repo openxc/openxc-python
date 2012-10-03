f93c03ab96c5d60d2d6c6d3a3503c4127bc75b3c
Christopher Peplin <cpeplin@ford.com>
2012-10-03 15:42:18 -0400

    Allow splitting by day, hour or trip.

--- openxc/tools/tracesplit.py
+++ openxc/tools/tracesplit.py
@@ -13,25 +13,13 @@ from .common import configure_logging
 LOG = logging.getLogger(__name__)
 
 
-class TraceSplitter(object):
-    MAXIMUM_RECORD_GAP_SECONDS = 600
-
+class BaseSplitter(object):
     def __init__(self):
         self.records = []
         self.buckets = defaultdict(list)
 
     def _key_for_record(self, record):
-        # TODO this does by trip, but also could do by day
-        timestamp = record['timestamp']
-        if getattr(self, 'last_timestamp', None) is not None:
-            if (timestamp - self.last_timestamp
-                    ) > self.MAXIMUM_RECORD_GAP_SECONDS:
-                self.current_trip_key = timestamp
-        else:
-            self.current_trip_key = timestamp
-        self.last_timestamp = timestamp
-        return datetime.datetime.fromtimestamp(self.last_timestamp).strftime(
-                "%Y-%m-%d-%H.%M")
+        raise NotImplementedError
 
     def split(self, files):
         for filename in files:
@@ -49,12 +37,42 @@ class TraceSplitter(object):
         self.records.append(message)
 
 
+class TimeSplitter(BaseSplitter):
+    def __init__(self, unit):
+        super(TimeSplitter, self).__init__()
+        self.unit = unit
+
+    def _key_for_record(self, record):
+        date = datetime.datetime.fromtimestamp(record['timestamp'])
+        if self.unit == "day":
+            date_format = '%Y-%m-%d'
+        elif self.unit == "hour":
+            date_format = '%Y-%m-%d-%H'
+        return date.strftime(date_format)
+
+
+class TraceSplitter(BaseSplitter):
+    MAXIMUM_RECORD_GAP_SECONDS = 600
+
+    def _key_for_record(self, record):
+        timestamp = record['timestamp']
+        if getattr(self, 'last_timestamp', None) is not None:
+            if (timestamp - self.last_timestamp
+                    ) > self.MAXIMUM_RECORD_GAP_SECONDS:
+                self.current_trip_key = timestamp
+        else:
+            self.current_trip_key = timestamp
+        self.last_timestamp = timestamp
+        return datetime.datetime.fromtimestamp(self.last_timestamp).strftime(
+                "%Y-%m-%d-%H.%M")
+
+
 def parse_options():
     parser = argparse.ArgumentParser(description="Receive and print OpenXC "
         "messages over USB")
     parser.add_argument("files", action="store", nargs='+', default=False)
     parser.add_argument("-s", "--split", action="store",
-            choices=['day', 'trip'])
+            choices=['day', 'hour', 'trip'], default='trip')
 
     arguments = parser.parse_args()
     return arguments
@@ -64,7 +82,11 @@ def main():
     configure_logging()
     arguments = parse_options()
 
-    splitter = TraceSplitter()
+    if arguments.split == 'trip':
+        splitter = TraceSplitter()
+    else:
+        splitter = TimeSplitter(arguments.split)
+
     for key, split in splitter.split(arguments.files).items():
         with open("%s.json" % key, 'w') as f:
             for record in split:
