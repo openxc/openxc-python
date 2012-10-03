9394c9a6e7404e89b656dcbe5c67744cb5a249fd
Christopher Peplin <cpeplin@ford.com>
2012-10-03 11:18:13 -0400

    Replay trace files with proper timing.

--- openxc/sources/trace.py
+++ openxc/sources/trace.py
@@ -1,8 +1,9 @@
 from __future__ import absolute_import
 
 import logging
+import time
 
-from .base import DataSource
+from .base import DataSource, DataSourceError
 
 LOG = logging.getLogger(__name__)
 
@@ -12,20 +13,62 @@ class TraceDataSource(DataSource):
 
     def __init__(self, callback, filename=None):
         super(TraceDataSource, self).__init__(callback)
+        self.filename = filename
+        self.reopen_file()
+
+    def reopen_file(self):
+        if getattr(self, 'trace_file', None) is not None:
+            self.trace_file.close()
+        self.trace_file = self.open_file(self.filename)
+
+    @classmethod
+    def open_file(cls, filename):
         try:
-            self.trace_file = open(filename, "r")
+            trace_file = open(filename, "r")
         except IOError as e:
             raise DataSourceError("Unable to open trace file %s" % filename, e)
         else:
             LOG.debug("Opened trace file %s", filename)
+            return trace_file
 
-    def start(self):
-        super(TraceDataSource, self).start()
-        self.trace_file.close()
+    def wait(self, starting_time, timestamp):
+        if getattr(self, 'first_timestamp', None) is None:
+            self.first_timestamp = timestamp
+            LOG.debug("Storing %d as the first timestamp of the trace file %s",
+                    self.first_timestamp, self.filename)
+
+        target_time = starting_time + (timestamp - self.first_timestamp)
+        time.sleep(max(target_time - time.time(), 0))
 
     def read(self):
-        # TODO play back in proper time by checking timestamp
         return self.trace_file.readline()
+
+    def run(self):
+        while True:
+            self.reopen_file()
+            starting_time = time.time()
+
+            while True:
+                line = self.read()
+                message, _, byte_count = self._parse_message(line)
+                if message is None:
+                    break
+
+                self.bytes_received += byte_count
+                if not self._validate(message):
+                    continue
+                if 'timestamp' in message:
+                    self.wait(starting_time, message['timestamp'])
+                self.callback(message)
+
+            self.trace_file.close()
+            self.trace_file = None
+
+    def _validate(self, message):
+        for key in ['name', 'value']:
+            if key not in message:
+                return False
+        return True
 
     def _write(self, message):
         raise NotImplementedError("Can't send commands to a trace file")
