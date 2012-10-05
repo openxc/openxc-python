a1c560ed94ebdb4337a841ed5f7c664b4b572b89
Christopher Peplin <chris.peplin@rhubarbtech.com>
2012-10-05 12:58:10 -0400

    Document openxc-trace-split.

--- openxc/tools/tracesplit.py
+++ openxc/tools/tracesplit.py
@@ -1,6 +1,12 @@
+"""
+This module contains the methods for the ``openxc-trace-split`` command line
+program.
+
+`main` is executed when ``openxc-trace-split`` is run, and all other callables
+in this module are internal only.
+"""
 from __future__ import absolute_import
 
-import logging
 import argparse
 import datetime
 from operator import itemgetter
@@ -9,8 +15,6 @@ from collections import defaultdict
 from openxc.formats.json import JsonFormatter
 from openxc.sources.trace import TraceDataSource
 from .common import configure_logging
-
-LOG = logging.getLogger(__name__)
 
 
 class BaseSplitter(object):
@@ -51,14 +55,15 @@ class TimeSplitter(BaseSplitter):
         return date.strftime(date_format)
 
 
-class TraceSplitter(BaseSplitter):
+class TripSplitter(BaseSplitter):
+    # TODO allow overriding this at the command line
     MAXIMUM_RECORD_GAP_SECONDS = 600
 
     def _key_for_record(self, record):
         timestamp = record['timestamp']
-        if getattr(self, 'last_timestamp', None) is not None:
-            if (timestamp - self.last_timestamp
-                    ) > self.MAXIMUM_RECORD_GAP_SECONDS:
+        last_timestamp = getattr(self, 'last_timestamp', None)
+        if last_timestamp is not None:
+            if (timestamp - last_timestamp) > self.MAXIMUM_RECORD_GAP_SECONDS:
                 self.current_trip_key = timestamp
         else:
             self.current_trip_key = timestamp
@@ -68,8 +73,8 @@ class TraceSplitter(BaseSplitter):
 
 
 def parse_options():
-    parser = argparse.ArgumentParser(description="Receive and print OpenXC "
-        "messages over USB")
+    parser = argparse.ArgumentParser(description="Split a collection of "
+            "OpenXC trace files by day, hour or trips")
     parser.add_argument("files", action="store", nargs='+', default=False)
     parser.add_argument("-s", "--split", action="store",
             choices=['day', 'hour', 'trip'], default='trip')
@@ -83,11 +88,11 @@ def main():
     arguments = parse_options()
 
     if arguments.split == 'trip':
-        splitter = TraceSplitter()
+        splitter = TripSplitter()
     else:
         splitter = TimeSplitter(arguments.split)
 
     for key, split in splitter.split(arguments.files).items():
-        with open("%s.json" % key, 'w') as f:
+        with open("%s.json" % key, 'w') as output_file:
             for record in split:
-                f.write(JsonFormatter.serialize(record) + "\n")
+                output_file.write(JsonFormatter.serialize(record) + "\n")
