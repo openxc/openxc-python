d0e872a74a823db78c42c18a414548108ab9798f
Christopher Peplin <chris.peplin@rhubarbtech.com>
2012-11-29 16:50:18 -0500

    Add argparse to dependencies for Python 2.6 users.

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ except ImportError:
 
 long_description = open('README.rst').read()
 
-install_reqs = ['pyusb', 'units >= 0.5', 'requests == 0.14.1']
+install_reqs = ['pyusb', 'units >= 0.5', 'requests == 0.14.1', 'argparse',]
 
 if sys.version_info < (3, 0):
     install_reqs.append('pyserial')
