05bb0c132d2360caa2bedf92513cfb0ad42e4f00
jacobhartsoch <jacobhartsoch@yahoo.com>
2013-09-20 09:47:56 -0700

    Progress on Python CAN Translator
    1) re-organized the generated data structures to be a bit more
    pythonesque, wrapping signals inside message and state inside signal.
    
    2) decodeCanMessage() and getMessageList() added to footer in generation
    of signals.py
    
    3) Grabbed canread.cpp from vi-firmware master and refactored into
    python.  This is required to support any sort of obfustication if we
    hope to release signals.pyc for specific vehicles.  Essentially,
    canread.pyc and signals.pyc combine to compartmentalize the translation
    of a raw can message to resulting OpenXC json completely.  A 3rd party
    hardware designer would simply:
    
    -import canread.pyc and signals.pyc for a specific vehicle
    -use two entry points:  decodeCanMessage() and getMessageList().  The
    second is to expose a list of CAN messages to setup any hardware
    filters.
    
    I wonder if this same approach could be used on the C side so Ford could
    start releasing just a few obj files per vehicle for 3rd party hardware
    designers instead of having to build a complete firmware for each
    vehicle?
    
    Status:
    Currently, we have a prototype hardware device with a python interpreter
    and an STN1100.  It is successfully setting up  CAN filtering (using a
    call to getMessageList() to get appropriate message ids), then
    processing each CAN message with a call into decodeCanMessage().  I'm
    currently faking some CAN data that matches the sample json but I'd love
    to get my hands on a json file for one of the cars we have access to or
    even just a few CAN messages to do further testing and research.
    
    Issues:
    
    1) None of the write handler capability is in place really yet
    
    2) for custom signal handlers, it is expected the producer of these
    handlers will return json instead of a value (different that the C
    version).  This is more flexible and also probably makes sense with
    loose typing.
    
    3) Not sure what exactly to do with message-level handlers (ie
    HandleSteeringWheelMessage)  I don't see in the example where they are
    supported in the current firmware.
    
    4) Assumes Big Endian.

--- openxc/generator/coder_py.py
+++ openxc/generator/coder_py.py
@@ -35,20 +35,41 @@ class CodeGeneratorPython(object):
 
     def build_source(self):
         lines = []
-        #lines.extend(self._build_header())
-#        lines.extend(self._build_extra_sources())
+        lines.extend(self._build_header())
+        lines.extend(self._build_extra_sources())
         lines.extend(self._build_message_sets())
         lines.extend(self._build_buses())
+#       lines.extend(self._build_signal_states()) #integrated into specific signal build now
         lines.extend(self._build_messages())
-        lines.extend(self._build_signal_states())
-        lines.extend(self._build_signals())
+        #lines.extend(self._build_signals())  #done internal to build_messages now
         lines.extend(self._build_initializers())
         lines.extend(self._build_loop())
         lines.extend(self._build_commands())
-#        lines.extend(self._build_decoder())
-#        lines.extend(self._build_filters())
+#       lines.extend(self._build_decoder()) #part of footer
+#       lines.extend(self._build_filters()) #part of footer
+        lines.extend(self._build_footer())
 
         return '\n'.join(lines)
+
+    def _build_extra_sources(self):
+        lines = []
+        for i, message_set in enumerate(self.sorted_message_sets):
+            for extra_source_filename in message_set.extra_sources:
+                with open(find_file(extra_source_filename, self.search_paths)
+                        ) as extra_source_file:
+                    lines.append(extra_source_file.read())
+        return lines
+
+    def _build_header(self):
+        with open(os.path.join(os.path.dirname(__file__),
+                'signals.py.header')) as header:
+            return [header.read().format(self.GENERATED_CODE_VERSION)]
+
+    def _build_footer(self):
+        with open(os.path.join(os.path.dirname(__file__),
+                'signals.py.footer')) as header:
+            return [header.read().format(self.GENERATED_CODE_VERSION)]
+
 
     @property
     def sorted_message_sets(self):
@@ -74,11 +95,12 @@ class CodeGeneratorPython(object):
         return max(len(list(message_set.active_signals()))
                 for message_set in self.message_sets)
 
+
     def _build_messages(self):
         lines = []
         lines.append("MAX_MESSAGE_COUNT = %d" %
                 self._max_message_count())
-        lines.append("CAN_MESSAGES = []")
+        lines.append("CAN_MESSAGES = {}")
 
         def block(message_set):
             lines = []
@@ -90,9 +112,13 @@ class CodeGeneratorPython(object):
                 LOG.info("Added message '%s'" % message.name)
                 lines.append("message = {}")
                 lines.append("message['bus_index']=%d" % message_set.lookup_bus_index(message.bus_name))
-                lines.append("message['id']=%d" % message.id)
+                lines.append("message['id']='%03X'" % message.id)
                 lines.append("message['name']='%s'" % message.name)
-                lines.append("CAN_MESSAGES.append(message)")
+                lines.append("message['signals']=[]")
+                
+                lines.extend(self._build_signals(message))
+                
+                lines.append("CAN_MESSAGES['%03X'] = message" % message.id)
                 lines.append("")
 
             return lines
@@ -143,41 +169,20 @@ class CodeGeneratorPython(object):
         return lines
 
 
-    def _build_signal_states(self):
+    def _build_signal_states_for_signal(self, signal):
         lines = []
-        lines.append("MAX_SIGNAL_STATES = %d" %
-                self.MAX_SIGNAL_STATES)
-        lines.append("MAX_SIGNAL_COUNT = %d" %
-                self._max_signal_count())
-        lines.append("SIGNAL_STATES= []")
-
-        def block(message_set, **kwargs):
-            states_index = 0
-            lines = []
-            for signal in message_set.active_signals():
-                 if len(signal.states) > 0:
-                    lines.append('states = []')
-                    for state_count, state in enumerate(signal.sorted_states):
-                        if state_count >= self.MAX_SIGNAL_STATES:
-                            LOG.warning("Ignoring anything beyond %d states for %s" %
-                                    (self.MAX_SIGNAL_STATES,
-                                        signal.generic_name))
-                            break
-                        lines.append("state = {}")
-                        lines.append("state['value']=%d" % state.value)
-                        lines.append("state['name']='%s'" % state.name)
-                        lines.append("states.append(state)")
-                        lines.append("")
-                    lines.append('SIGNAL_STATES.append(states)')
-                    signal.states_index = states_index
-                    states_index += 1
-            return lines
-
-        lines.extend(self._message_set_lister(block))
-
-        lines.append("")
-
+        lines.append('states = []')
+        for state_count, state in enumerate(signal.sorted_states):
+            if state_count >= self.MAX_SIGNAL_STATES:
+                LOG.warning("Ignoring anything beyond %d states for %s" % (self.MAX_SIGNAL_STATES, signal.generic_name))
+                break
+            lines.append("state = {}")
+            lines.append("state['value']=%d" % state.value)
+            lines.append("state['name']='%s'" % state.name)
+            lines.append("signal['states'].append(state)")
+            lines.append("")
         return lines
+
 
     def _message_set_lister(self, block, indent=4):
         lines = []
@@ -196,14 +201,14 @@ class CodeGeneratorPython(object):
         return lines
 
 
-    def _build_signals(self):
+    def _build_signals(self, message):
         lines = []
-        lines.append("SIGNALS = []")
+        #lines.append("SIGNALS = []")
 
         def block(message_set):
             lines = []
             i = 1
-            for signal in message_set.all_signals():
+            for signal in message.sorted_signals():
                 if not signal.enabled:
                     LOG.warning("Skipping disabled signal '%s' (in 0x%x)" % (
                         signal.generic_name, signal.message.id))
@@ -213,7 +218,7 @@ class CodeGeneratorPython(object):
                 lines.append("signal['message_set_index']=%d" % signal.message_set.index)
                 lines.append("signal['message_index']=%d" % message_set.lookup_message_index(signal.message))
                 lines.append("signal['generic_name']='%s'" % signal.generic_name)
-                lines.append("signal['bit_position']='%s'" % signal.bit_position)
+                lines.append("signal['bit_position']=%d" % signal.bit_position)
                 lines.append("signal['bit_size']=%d" % signal.bit_size)
                 lines.append("signal['factor']=%f" % signal.factor)
                 lines.append("signal['offset']=%f" % signal.offset)
@@ -221,15 +226,20 @@ class CodeGeneratorPython(object):
                 lines.append("signal['max_value']=%f" % signal.max_value)
                 lines.append("signal['frequency']=%d" % signal.send_frequency)
                 lines.append("signal['send_same']=%s" % signal.send_same)
+                lines.append("signal['handler']=%s" % signal.handler)
+                lines.append("signal['received']=False")
+                lines.append("signal['sendClock']=0")
+                lines.append("signal['lastValue']=0")
 
                 if (len(signal.states) > 0):
-                    lines.append("signal['state_count']=%d" % len(signal.states))
-                    lines.append("signal['states_index']=%d" % signal.states_index)
+                    lines.append("")
+                    lines.append("signal['states'] = []")
+                    lines.extend(self._build_signal_states_for_signal(signal))
 
                 lines.append("signal['writable']=%s" % str(signal.writable).title())
-                lines.append("signal['write_handler']='%s'" % signal.write_handler or "NULL")  #handlers are implemented outside of this module for now
+                lines.append("signal['write_handler']=%s" % signal.write_handler or "NULL")  #handlers are implemented outside of this module for now
                 lines.append("signal['name']='%s'" % signal.name)
-                lines.append("SIGNALS.append(signal)")
+                lines.append("message['signals'].append(signal)")
                 lines.append("")
 
                 LOG.info("Added signal '%s'" % signal.generic_name)
