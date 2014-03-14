4739a1a58bb3cda34776fed3834653e35ad0c797
Christopher Peplin <chris.peplin@rhubarbtech.com>
2014-03-14 15:38:55 -0400

    Print receive messages when using openxc-diag tool.

--- openxc/tools/diagnostics.py
+++ openxc/tools/diagnostics.py
@@ -3,7 +3,12 @@ from __future__ import absolute_import
 import argparse
 import time
 
+from openxc.formats.json import JsonFormatter
 from .common import device_options, configure_logging, select_device
+
+def receive(message, **kwargs):
+    message['timestamp'] = time.time()
+    print(JsonFormatter.serialize(message))
 
 
 def diagnostic_request(arguments, controller):
@@ -48,8 +53,7 @@ def main():
     arguments = parse_options()
 
     controller_class, controller_kwargs = select_device(arguments)
-    controller = controller_class(**controller_kwargs)
+    controller = controller_class(receive, **controller_kwargs)
     controller.start()
 
     diagnostic_request(arguments, controller)
-    time.sleep(1)
