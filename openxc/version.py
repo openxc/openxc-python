5c28a8c4b8d104d636045b2f74da5942f071c7f5
Christopher Peplin <chris.peplin@rhubarbtech.com>
2014-04-29 00:13:09 -0400

    Bump to next development release.

--- openxc/version.py
+++ openxc/version.py
@@ -6,7 +6,7 @@ problems with ``__init__.py`` (which is loaded by setup.py during installation,
 which in turn needs access to this version information.)
 """
 
-VERSION = (0, 11, 0)
+VERSION = (0, 11, 1)
 
 __version__ = '.'.join(map(str, VERSION))
 
