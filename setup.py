13951b2af95490fb85bc50abbedc7e16519eeffd
pjt0620 <pjt0620@gmail.com>
2019-05-10 11:58:42 -0400

    Add curses in as a requirement for windows

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ except ImportError:
 long_description = open('README.rst').read()
 
 install_reqs = ['pyusb==1.0.0a3', 'units >= 0.5', 'argparse', 'requests==2.20.0',
-        'protobuf==2.6.1']
+        'protobuf==2.6.1'] + ["windows-curses >= 1.1"] if "win" in sys.platform else [],
 
 setup(name='openxc',
     version=get_version(),
