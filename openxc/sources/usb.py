31c37c2b3483f475ae9ed1e17a9558db4511dc42
pjt0620 <ptreman@ford.com>
2019-09-16 11:20:08 -0400

    Fix sting encoding issues

--- openxc/sources/usb.py
+++ openxc/sources/usb.py
@@ -82,9 +82,9 @@ class UsbDataSource(BytestreamDataSource):
             read_size=DEFAULT_READ_REQUEST_SIZE):
         timeout = timeout or self.DEFAULT_READ_TIMEOUT
         try:
-            return self.device.read(0x80 + endpoint_address,
+            str(return self.device.read(0x80 + endpoint_address,
                     read_size, self.DEFAULT_INTERFACE_NUMBER, timeout
-                    ).tostring()
+                    ),'utf-8')
         except (usb.core.USBError, AttributeError) as e:
             if e.errno == 110:
                 # Timeout, it may just not be sending
