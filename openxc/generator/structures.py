db1e751a019ff8e80fe5ec416b99d40d7eb0aae4
Sama <vsama@ford.com>
2019-11-18 16:55:49 -0500

    fix the duplicate error

--- openxc/generator/structures.py
+++ openxc/generator/structures.py
@@ -161,9 +161,6 @@ class Message(object):
                     states.append(SignalState(raw_match, name))
             signal_data.pop('states', None)
 
-            if signal_name in self.signals:
-                fatal_error('ERROR: Signal %s in %s has more than one definition' % (signal_name, self.name))
-
             signal = self.signals[signal_name]
             signal.name = signal_name
             signal.message_set = self.message_set
@@ -172,7 +169,6 @@ class Message(object):
             # obvious clean solution to it at the moment
             signal.states = states
             signal.merge_signal(signal_data)
-
 
     def validate(self):
         if self.bus_name is None:
--- openxc/utils.py
+++ openxc/utils.py
@@ -89,11 +89,21 @@ def find_file(filename, search_paths):
     fatal_error("Unable to find '%s' in search paths (%s)" % (
             filename, search_paths))
 
+# https://stackoverflow.com/a/14902564
+def dict_raise_on_duplicates(ordered_pairs):
+    """Reject duplicate keys."""
+    d = {}
+    for k, v in ordered_pairs:
+        if k in d:
+           raise ValueError("duplicate key: %r" % (k,))
+        else:
+           d[k] = v
+    return d
 
 def load_json_from_search_path(filename, search_paths):
     with open(find_file(filename, search_paths)) as json_file:
         try:
-            data = json.load(json_file)
+            data = json.load(json_file, object_pairs_hook=dict_raise_on_duplicates)
         except ValueError as e:
             fatal_error("%s does not contain valid JSON: \n%s\n" %
                     (filename, e))
