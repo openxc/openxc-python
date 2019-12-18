085dbb435d02c690b80881fa608d5e3125838e65
Baldwin <CBALDW19@ford.com>
2019-12-18 15:56:52 -0500

    Added settings for dashboard

--- openxc/tools/static/js/dashboard.js
+++ openxc/tools/static/js/dashboard.js
@@ -1,11 +1,18 @@
 let dataPoints = {};
 
+/* --- Dashboard parameters ------- */
+let justChangedHighlightDuration;
+let recentlyChangedHighlightDuration;
+/* --- End dashboard parameters --- */
+
 $(document).ready(function() {
+	updateDashboardParameters();
+
     namespace = '';
     var socket = io(namespace);
     socket.on('vehicle data', function(msg, cb) {
         // console.log(msg);
-        
+
         if (!(msg.name in dataPoints)) {
         	dataPoints[msg.name] = {
         		current_data: undefined,
@@ -26,6 +33,16 @@ $(document).ready(function() {
             cb();
     });
 });
+
+function updateDashboardParameters() {
+	valueChangedTimer = Number($('#justChangedHighlightDuration').val());
+	valueRecentlyChangedTimer = Number($('#recentlyChangedHighlightDuration').val());
+}
+
+function saveSettings(e) {
+	e.preventDefault();
+	updateDashboardParameters();
+}
 
 function addToDisplay(msgName) {
 	// Insert new rows alphabetically
@@ -74,8 +91,49 @@ function updateDisplay(dataPoint) {
 	}
 
     $('#' + msg.name + '_value').text(msg.value);
+    highlightCell('#' + msg.name + '_value');
+
     $('#' + msg.name + '_num').text(dataPoint.messages_received);
     $('#' + msg.name + '_freq').text(Math.ceil(1 / dataPoint.average_time_since_update));
+}
+
+function highlightCell(cellId) {
+	$(cellId).stop(true);
+    $(cellId).css({'background': '#1338F0', 'color': 'white'});
+    $(cellId).animate({backgroundColor: '#949494'}, valueChangedTimer, function() {
+    	$(this).animate({backgroundColor: '#FFFFFF', color: 'black'}, valueRecentlyChangedTimer);
+    });
+}
+
+function validateSettingsForm() {
+	let valid = true;
+	let errors = [];
+
+	$('.error').each(function() {
+		$(this).text('');
+	});
+
+	errors = validateTimerInput($('#justChangedHighlightDuration'), errors);
+	errors = validateTimerInput($('#recentlyChangedHighlightDuration'), errors);
+
+	if (errors.length > 0) {
+		valid = false;
+		errors.forEach(function(e) {
+			$('#' + e.id + '_error').text(e.msg);
+		});
+	}
+
+	return valid;
+}
+
+function validateTimerInput(input, errors) {
+	let inputVal = input.val();
+	
+	if (isNaN(inputVal) || inputVal < 0) {
+		errors.push({id: input[0].id, msg: 'Input must be a positive number'});
+	}
+
+	return errors;
 }
 
 function updateDataPoint(dataPoint, measurement) {
